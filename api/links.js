d04c92c2b3e8038f93fc8db329713103d2fd3e76
Jana <109353868+janybuny9@users.noreply.github.com>
2024-12-27 17:05:54 +0100

    Update links.js

--- api/links.js
+++ api/links.js
@@ -1,28 +1,129 @@
-// API für das Abrufen und Hinzufügen von Links
-export async function fetchLinksFromAPI() {
-  try {
-    const response = await fetch("https://janybuny9.pythonanywhere.com/get_refs");
-    if (!response.ok) throw new Error("Fehler beim Abrufen der Links");
-    return await response.json();
-  } catch (error) {
-    console.error("Error fetching links:", error);
-    return [];
+let links = [
+  "https://www.pond0x.com/swap/solana?ref=7WTPWhjEfyDrJA5crD9naahKgMDKQUqoXTMpLW4T3kkGkmUFVWsFj2opZoM",
+  "https://www.pond0x.com/swap/solana?ref=Nvgbw4go7cRAiPhSvHULC7DwwhcTf5N12udL6yFwiUoERh1Ny1SFoW5wKD8W",
+  "https://www.pond0x.com/swap/solana?ref=Kghj7EiEguu1zBGo4Y5W23Txm2G7gHf8LYcGDMf8RSsqC7CNLb1vRLLCtiUm",
+  "https://www.pond0x.com/swap/solana?ref=J35p6BX3cX26S72qtQ3MnzApSYELmMhMRFsTzhokbCaGwXk5nynDoP9YNEyT",
+  "https://www.pond0x.com/swap/solana?ref=JMeKKttDJCctKBtQchTcL5ggUWTU98czT5KwZGMrrxjtnqaa13Toc34ajhFe",
+  "https://www.pond0x.com/swap/solana?ref=JP7XyLZ2QccPGJceekCjQsopPsNh7HZhYAwAXbW6nGo4MnxnqvwdNWPpwXcm",
+  "https://www.pond0x.com/swap/solana?ref=JNxZD2Xj7X6nsjcbz2jv4yv11vD1APSBEG3v4ChwYiWJS6pVPbYEZu2FyEUT",
+  "https://www.pond0x.com/swap/solana?ref=JNkh1E85PoBRUGae65uDmY5JzFFnxg9iVHH7rQq6snV7EeN3p4kK63MgUt9G",
+  "https://www.pond0x.com/swap/solana?ref=7aTaUrhrwwvE9puSEg8ZwAbmzfszxsFnGKj5dQ3BLjw2SzhWRKhUmWp9VpK",
+  "https://www.pond0x.com/swap/solana?ref=QVekUVJjxS7GvspNjkDK1TiR18ZSgPqA22N8hAzWvPPAKKr6PrTH6gTyfMsu",
+  "https://www.pond0x.com/swap/solana?ref=NcGTRZiaB1k43D5n6BPDfEBi7BZQfSPt592HKWjDVt3HsoxCqudWo8LSQXwg",
+  "https://www.pond0x.com/swap/solana?ref=J1AE9uSpFL2iavRSrrkVi8fNFvpYPqNDcHFGEwN1DooB51bShPcuwa677zGU",
+  "https://www.pond0x.com/swap/solana?ref=HLsCqmt1jg2ro9LVfFwGJ9FB9Xu65CRLqvdysAW3v4LmskiTk1FQRS4xZcSu",
+  "https://www.pond0x.com/swap/solana?ref=HMp1pgV3aJd1ywXGtSkY3DR9EbbctzyTueUGwUXXAvXLvMsNAMbp2N87kKed",
+  "https://www.pond0x.com/swap/solana?ref=JfgkFQYQexRQkBWf5Zuh9th4A23eEGrnRaKJJAsKXDP5wcvH8akxG1KeX1GD",
+  "https://www.pond0x.com/swap/solana?ref=KKBZD1SPHAbeGDogG5xLFpsyVXzUL6eyTjfMT6J3ZimxPv8P86b1VCAjTcw2",
+  "https://www.pond0x.com/swap/solana?ref=NEQ3GNBYE1THPGmefaPM9FM4BDkw6FvwRePaQE7MMpH5pLuYaxgNiFsFM9um",
+  "https://www.pond0x.com/swap/solana?ref=R8RXn3CF5wHYRU61shcNu7FuiHDPiCgmBaTTYLt9czoprKJVQ73wsV2jaoC4",
+  "https://www.pond0x.com/swap/solana?ref=HfiyCFx9hNDtSoW6DUb4ofNc77cbi1NSe2RLXSUjg9jeEGYdHQPZEdfLRptD",
+  "https://www.pond0x.com/swap/solana?ref=Nri2C7eZkTszHUpJgVqyzoa4RSsTMCxpRAm3W5Kx74ucJqX8JdT8tkV2njGe",
+  "https://www.pond0x.com/swap/solana?ref=6YH2AViUbJi5dKxoJ5azufxCUeFkLRTPsLHmu5CyZQ5LNPsJZ68wT4ZzxAx",
+  "https://www.pond0x.com/swap/solana?ref=PrAHtgSZghqdTeFCzTsz89AQnECrtUzydcuZZ7rYjV8wQCNVzjbzofLB25oJ",
+  "https://www.pond0x.com/swap/solana?ref=HjgiNrkmFBUZHcTEfrP4qtUBeBbnVJpA5S8kE2ZfRWkQ44pHHo1F3pkZfeoV",
+  "https://www.pond0x.com/swap/solana?ref=QA188Tqs5TwLLNnB5WpwPweP98WfKc35YfE2X21nvnqKtShxB93eABoBhnL9",
+  "https://www.pond0x.com/swap/solana?ref=Nc4NYu8kbvrQsgG3fooWGcJpZy4zpRsfQ3FvvhwiCQF325wR9cGceVgm3MfX",
+  "https://www.pond0x.com/swap/solana?ref=JzqcjEC91odtCnMvwkikX4uTanu9RGuDsJCCcTT4YPEQycKS6MUQ95Ttz9Hz",
+  "https://www.pond0x.com/swap/solana?ref=PDhzvTjJM5baci7MDqJ9Fa6xiSwdkESc6FMuYxy7MvJjNEpvXp8jyRwDD9pW",
+  "https://www.pond0x.com/swap/solana?ref=QDPKwbHgkWDAjbF42TJJntgUnR4irhQSVUnkg77a6SNntmfAg4vC4eA6LMSh",
+  "https://www.pond0x.com/swap/solana?ref=HM69CZYe1tYrQ2MsJR8mczeZKqft7DHtMU3YG1wNJ2aCAv7ix2LDR2y4Ew1b",
+  "https://www.pond0x.com/swap/solana?ref=Hfs6PPd9jXsuhyquEgqSqNZH3sZV5YseyAx16UKmrA3oDjgWa861LhUkzuTm",
+  "https://www.pond0x.com/swap/solana?ref=JhAhUSm4Y8rzTt2kKWhbJZXoAbiYiGG81YR8hLNTCY9mpKMXKqDXxRgpU4kV",
+  "https://www.pond0x.com/swap/solana?ref=PYM4TgjazDkm3M4ts7NZUchBQEvQ5aS7Fm5RM3THARihpR3tMDnc9DrBtRJ6",
+  "https://www.pond0x.com/swap/solana?ref=Jy9TwxBp5WeYeoCJMBaBMX5NfsBqdxnL2jwRJgskscZaSa8EeUgLN5HA5KAv",
+  "https://www.pond0x.com/swap/solana?ref=HMpbe211u4uAu9TseyYhBC9ZYdkMMuaqGpZMLYTPFf9sQvDvqHpgBJXYWoxJ",
+  "https://www.pond0x.com/swap/solana?ref=PDGZgYRq13wwwgsqVJ8yBXvd8x2iAMN649CzH2Z21Q8VgcFR2v5x5qeu2DPW",
+  "https://www.pond0x.com/swap/solana?ref=K1tBUTLRk5Ndkq6B3EVKEQF1WMorHyRxDUJBoSnBKUU4GYBCtyQQJnoAPbF1",
+  "https://www.pond0x.com/swap/solana?ref=KbYEecdVXkrXx19jvtFDKNkpikK9ztsvSJZGL2sVXmpaaKSdM6RrWWtenEML",
+  "https://www.pond0x.com/swap/solana?ref=KfDtgjDSkX2UTpTJK7LgEvRSJm3LvH2r7ApwJWWGXh6KWQv3QmdWFodiSEcD",
+  "https://www.pond0x.com/swap/solana?ref=6dbsCj5Pe1iRrcmYCiw86BR36PtRqioAVdCadJeNSWNn9DMsFT8hyDN81Ln",
+  "https://www.pond0x.com/swap/solana?ref=PCtYBTDftoDUzLD7MwS6cm1NqkMgEzUjmxGXHNSL4wVMnCgpF6rCkso5MQr5",
+  "https://www.pond0x.com/swap/solana?ref=PBG6L2nFG7YNQav1tAuLxg4ET6Ph1hNcSqxoSoyefHXF1meVi8zGSuk7ubGj",
+  "https://www.pond0x.com/swap/solana?ref=9eKmBsUbxsXBCP7MHQaVqt2dQerM5q2LsF9ro6LJpVyR871epMeZW4EpiKs",
+  "https://www.pond0x.com/swap/solana?ref=PZkPggK1yMqDDD3CwQqbcrC5bbhZ257RoEXye4JUSbaRTCZXZVM4M4SjF5jA",
+  "https://www.pond0x.com/swap/solana?ref=NGXvmdDfpGV5E9XAfJCnFBfobXQBiSu6DVpDcQVs5hRL1T68aAnsb2xRXVp6",
+  "https://www.pond0x.com/swap/solana?ref=PrK1L912MMxQQZzJmgiW3r6j12VPCwVDa1Rmwox7npCRN5daSKjQUBfJjBHC",
+  "https://www.pond0x.com/swap/solana?ref=QZCM5qumkyw2KMwvQxTFrhPBXrHeuj2Sb9Uz59LEtAx2JHQzPM6Tac2zwPeG",
+  "https://www.pond0x.com/swap/solana?ref=Kbu5XTijJxiUivs8zX9KbSRSpp89Pzd3cfNtWbv9P5tfEceAqvevTqGqmH4R",
+  "https://www.pond0x.com/swap/solana?ref=R8RUyk939fYqE8BQzw6rzpRmEcLzFoHRBwhsYMXXMNGaH86uo4poe67BDvED",
+  "https://www.pond0x.com/swap/solana?ref=JPRPWi6Me4p8mCDb9v2DtWHHP4HwTnYEZ2NwJ6DaDXKFq9LY432pLG2UW1Ab",
+  "https://www.pond0x.com/swap/solana?ref=QUUSNPCuSSUqShcwyT58pfrqzyw79TiVpVyBU2JQH8pz15Z6Tcwa8rkDoiJZ",
+  "https://www.pond0x.com/swap/solana?ref=JhXpwc9Vtr2GyqJaY2DQwewVM25QQbwP4HzfcJXzcYDtvsWJQcupHpwbhbje",
+  "https://www.pond0x.com/swap/solana?ref=J3Sd58wv9szFRRTrzT3cuRGU5WCJfpqPgVVri4pgHuu66JjjQxDjqaCKEvHP",
+  "https://www.pond0x.com/swap/solana?ref=KJE8ZYwcv1GX7yBzSSYMq4KLcKTRb1iYgZWSuMKqeq35diUz4Zhv5aRNZN9e",
+  "https://www.pond0x.com/swap/solana?ref=Nu9JEqt2pBh6SjonAo5ghQFoZok94xgxwQAhh9RbG2yn6C7E8HLthPdrsB67",
+  "https://www.pond0x.com/swap/solana?ref=Ps8Uzpz9xEndKWn3fd8veVqbtczE1a4NjhRZM7tPssAcSJWYgiygho3EyADf",
+  "https://www.pond0x.com/swap/solana?ref=JKGxho4PDJYDVRbyGaNjUPBxKGaxHAoyHtQB9wTvHyUSR2SSoJRmFNQCEmfc",
+  "https://www.pond0x.com/swap/solana?ref=NGtnaqb14QnQVYGj2JXYbhn1Cy5eNJ2AhvS87EtAHt1tpwfJPFnTNdV2P1tj",
+  "https://www.pond0x.com/swap/solana?ref=QZGT8zmC7t6CZNW1vzZeBucL8qUFkFMkXg3TgbTYDVv661UzP8akVWzvx4QG",
+  "https://www.pond0x.com/swap/solana?ref=PCb6dMwWnizuCdCM8yD4Hmt7k2mvZyEjajiWVWUZtYfkk64G29BMqn4Zd3Zo",
+  "https://www.pond0x.com/swap/solana?ref=JNgTyURiVLH2MkrryBDM4Uwm2Q7apuunALCiiEJ5QRQuEWhBifmVSsUvPteQ",
+  "https://www.pond0x.com/swap/solana?ref=PVzE1S4y795R9BHQodDDm5qvrJExw5jjTgKkHKYXcbUrv2MXo525MAevRd49",
+  "https://www.pond0x.com/swap/solana?ref=KJ1FBXmU8yVqtca4eBKYm9oLuzzjakSnAjwjohgSUk1fjLFrXvntMgtwWMSp",
+  "https://www.pond0x.com/swap/solana?ref=NH2zvd4k32hhNzegCG8nq4tcP2qUwLTK3juLgKcUqhD7CeKQr7YaqWPSKUnP",
+  "https://www.pond0x.com/swap/solana?ref=HL3C51XbF1YvidQ3NbVdKj9tWJDXhk77uPUpSPhq6xeozGQSZugyk1wGvi6H",
+  "https://www.pond0x.com/swap/solana?ref=QZCp1atjuxpa5PhXPqrxa1WUaAk32gMwDPr95rRmwsGZ9PFQdbhZJtXycDbS",
+  "https://www.pond0x.com/swap/solana?ref=KMr3uBHAVKMEYFSABb9wBwy6T2nvDWx57JisXZmnYR8Y58c9Z2ALCoUSGM4C",
+  "https://www.pond0x.com/swap/solana?ref=K2koXZhkiJpNh29DsSFMxkQikKuv6ma6z2hVhykMN5KcoaqumFEsTRMRNpZj",
+  "https://www.pond0x.com/swap/solana?ref=NbgRrZPfHtw6XvwQJXfyXhE5NoA4StDeia9vCpf865w6krTu9W4kuea3yCAD",
+  "https://www.pond0x.com/swap/solana?ref=8v23tpjs4rtUmwTHnKvVqrP9KVnQj3moiaYwCFQEc8JVokwMnETKjRtScNt",
+  "https://www.pond0x.com/swap/solana?ref=8Pk8bfmabY5qSYVtdP32MgxNCF8MVtoDEmxheB5ijbYmzeqEzP3XqvNazWw",
+  "https://www.pond0x.com/swap/solana?ref=QBB7n2EdT7Yc1HSnMuvwQDTtcTCrUoLMgLo8hd9ETk1pjTX78p3oQdejYYDP",
+  "https://www.pond0x.com/swap/solana?ref=QEeJhv5e5hZbkoZdnCJ7Df8izesNtos7UsUHCLBcMwAYnSrTyBDPrtAi1dzB",
+  "https://www.pond0x.com/swap/solana?ref=NceBw3ordXQqnTLBjz4Vd47kB83d7gwENLFc1Ssgn93dWzeggtHANZ6XHiCF",
+  "https://www.pond0x.com/swap/solana?ref=KdEX2UxG5VWXS3jddWsuQthGt1RvA5hT6Kpv5meezdskgkNEtatWCYGhysa1",
+  "https://www.pond0x.com/swap/solana?ref=NHRTsuJC3vRmv15ofmC1kjTJp61feU5GBLFFEwkdAtahFvnnwRhzGTbw3h8j",
+  "https://www.pond0x.com/swap/solana?ref=QWQ1QisY3qbXTjagoj8QhKeH8cfqwVbaryv6u1JNtDbijjFfpe63pQyzMVM9",
+  "https://www.pond0x.com/swap/solana?ref=NGyVZFv8g7GsMXyG15eZ18qtHWT2RUVPXfA3sx2zzhTHBUMwx4LuZYTYTX6G",
+  "https://www.pond0x.com/swap/solana?ref=PD2u3ces8b7zQB7wWPEfn5KMrc7oC37zCpePWtB9gTpJ9eoPAzHA6s6aqGjR",
+  "https://www.pond0x.com/swap/solana?ref=QXMx76hzzTSxQze6BshHzfZ9rPW4kwTMoRym1nzfKthjVhghZtcQsqUrS2FY",
+  "https://www.pond0x.com/swap/solana?ref=Jwtfj6zJyQMqsGP6Za84ccfdqhGAESrnCa9rEGEGUKVz1ad9ej3rLwNAiTaq",
+  "https://www.pond0x.com/swap/solana?ref=NDjGmiLxBv31htANZwxF3vyV9oev9p74ZDxSLMnmy7Kd5SQYys1wXUntddfK",
+  "https://www.pond0x.com/swap/solana?ref=Hi5e5g7vutBuRiBoiJ7nKUj4vkURHPDwDeEBuvW68JXdSMedF1ZU4u7QSfRG",
+  "https://www.pond0x.com/swap/solana?ref=QZCCPeteQsKUi67vk9iPozAmBQ6E3SeQVkrFtRR5tchNjnJsdk2kBavhSoqQ",
+  "https://www.pond0x.com/swap/solana?ref=NHFhRtk43dqN3pA8uP2d9GmSj7nTXH1qtDEzANAMvrRPquXQ7Dh2KU1Jtni1",
+  "https://www.pond0x.com/swap/solana?ref=J2nKabtHGsRQ7887mXrSqpFPs9W1dsidUHQ8Q6cBEQNQq8fNYQrMNS4Uj7Vg",
+  "https://www.pond0x.com/swap/solana?ref=PtP2sAw2LTYXnEDwokAADJLvjKtk9AKricV6ShoPeGKYayh9M2H4QzwXHzwR",
+  "https://www.pond0x.com/swap/solana?ref=HiadU997LPFnJfjG193QpauuRDYBiUcSAy8Tkr22pC3Q1gNC3g2d2kSZi7rz",
+  "https://www.pond0x.com/swap/solana?ref=QYU87ALYEGF87qx57WAqVCSNEi87ZBCMEbk6BKQ5jy1j1xjr6UiNChvRte9R",
+  "https://www.pond0x.com/swap/solana?ref=K2P4AvyZFsFSXgNZdr1BeYXWwZLCNxPoBN1wDcvoEH4kkV3mnqyaEvXqYxwX",
+  // Füge hier weitere Links hinzu, falls gewünscht
+];
+
+export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
   }
-}
 
-export async function addLinkToAPI(newLink) {
-  try {
-    const response = await fetch("https://janybuny9.pythonanywhere.com/add_ref", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ ref_link: newLink }),
-    });
+  if (req.method === "GET") {
+    // Rückgabe aller Links
+    res.status(200).json(links);
+  } else if (req.method === "POST") {
+    const { link } = req.body;
 
-    if (response.status === 409) return { error: "Link existiert bereits" };
-    if (!response.ok) throw new Error("Fehler beim Hinzufügen des Links");
-    return await response.json();
-  } catch (error) {
-    console.error("Error adding link:", error);
-    return { error: "Fehler beim Hinzufügen des Links" };
+    // Validierung des Links
+    if (!link || !link.startsWith("https://www.pond0x.com/swap/solana?ref=")) {
+      res.status(400).json({ error: "Ungültiger Link" });
+      return;
+    }
+
+    // Überprüfung auf Duplikate
+    if (links.includes(link)) {
+      res.status(409).json({ error: "Link existiert bereits" });
+      return;
+    }
+
+    // Hinzufügen des Links
+    links.push(link);
+    res.status(201).json({ link });
+  } else {
+    res.setHeader("Allow", ["GET", "POST", "OPTIONS"]);
+    res.status(405).end(`Methode ${req.method} ist nicht erlaubt`);
   }
 }
